ea2f20cbcff7b84f4f6353bcd5c0de6213b3122f
Guilherme <guilherme.dds.dev@gmail.com>
2025-04-14 11:25:41 -0300

    Add Dashboard

--- dashboard.py
+++ dashboard.py
@@ -2,54 +2,57 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+#Configurações da página
 st.set_page_config(
     page_title='Formula 1 | Dashboard',
     page_icon='images/f1_logo.png',
     layout='wide'
 )
 
+#Cabeçalho:
 col1, col2 = st.columns([1, 10])
 with col1:
     st.image('images/f1_logo.png', width=100)
 with col2:
     st.markdown('# DASHBOARD', unsafe_allow_html=True)
 
+#Carregando as bades de dados:
 df_drivers = pd.read_csv("data/drivers.csv", sep=",")
 df_standings = pd.read_csv("data/driver_standings.csv", sep=",")
 df_races = pd.read_csv("data/races.csv", sep=",")
 df_results = pd.read_csv("data/results.csv", sep=",")
 
-# Create full name column
+#Selecionando o piloto:
 df_drivers['full_name'] = df_drivers['forename'] + ' ' + df_drivers['surname']
 driver = st.sidebar.selectbox("Piloto", df_drivers["full_name"].unique(), index=None, placeholder="Selecione um piloto")
 
-# Filter and sum points for selected driver across all races
+#Calculando o total de pontos:
 if driver:
     driver_id = df_drivers[df_drivers['full_name'] == driver]['driverId'].iloc[0]
-    # Get all results for the selected driver
     driver_results = df_results[df_results['driverId'] == driver_id]
-    # Calculate total points
     total_points = driver_results['points'].sum()
 else:
     total_points = 0
 
+#Dados do piloto(Por enquanto só o total de pontos):
 col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.metric(label="Total de Pontos", value=f"{total_points:.0f}")
 
-
-# Create points per year graph
+#Gráfico de pontos por ano do piloto:
 if driver:
-    # Merge results with races to get year information
     driver_points_by_year = (
+        #Filtro para pegar apenas o ano em que o piloto correu:
         df_results[df_results['driverId'] == driver_id]
+        #Coleta o ano da corrida:
         .merge(df_races[['raceId', 'year']], on='raceId')
+        #Agrupa os dados por ano e soma os pontos das corridas:
         .groupby('year')['points']
         .sum()
         .reset_index()
     )
-    
-    # Create line chart
+
+    #Criação do gráfico usando Plotly Express:
     fig = px.line(
         driver_points_by_year,
         x='year',
@@ -57,10 +60,12 @@ if driver:
         title=f'Pontos por ano - {driver}',
         markers=True
     )
+    #Atualização do gráfico:
     fig.update_layout(
         xaxis_title="Ano",
         yaxis_title="Pontos",
         showlegend=False
     )
     
+    #Exibição do gráfico usando Streamlit:
     st.plotly_chart(fig, use_container_width=True)
