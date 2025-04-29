72ea62065a6b221a27932bb0d60a4434f9ec445a
Guilherme <guilherme.dds.dev@gmail.com>
2025-04-29 12:30:15 -0300

    Atualização

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,5 @@
+#Observação: Eu optei por colocar as variáveis em inglês pra ficar mais facil de entender já que a base de dados está em inglês.
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -16,56 +18,70 @@ with col1:
 with col2:
     st.markdown('# DASHBOARD', unsafe_allow_html=True)
 
-#Carregando as bades de dados:
+#Carregando as bases de dados:
 df_drivers = pd.read_csv("data/drivers.csv", sep=",")
 df_standings = pd.read_csv("data/driver_standings.csv", sep=",")
 df_races = pd.read_csv("data/races.csv", sep=",")
 df_results = pd.read_csv("data/results.csv", sep=",")
+df_constructors = pd.read_csv("data/constructors.csv", sep=",")
 
-#Selecionando o piloto:
+#Criando o menu de seleção
 df_drivers['full_name'] = df_drivers['forename'] + ' ' + df_drivers['surname']
-driver = st.sidebar.selectbox("Piloto", df_drivers["full_name"].unique(), index=None, placeholder="Selecione um piloto")
+category = st.sidebar.selectbox("Categoria", ["Piloto", "Equipe"], index=None, placeholder="Selecione uma categoria")
 
-#Calculando o total de pontos:
-if driver:
-    driver_id = df_drivers[df_drivers['full_name'] == driver]['driverId'].iloc[0]
-    driver_results = df_results[df_results['driverId'] == driver_id]
-    total_points = driver_results['points'].sum()
-else:
-    total_points = 0
+#Coletando as informações do piloto selecionado:
+if category == "Piloto":
+    driver = st.sidebar.selectbox("Piloto", df_drivers["full_name"].unique(), index=None, placeholder="Selecione um piloto")
+    if driver:
+        driver_info = df_drivers[df_drivers['full_name'] == driver].iloc[0]
+        driver_id = driver_info['driverId']
+        driver_nationality = driver_info['nationality']
+        
+        driver_results = df_results[df_results['driverId'] == driver_id]
+        total_points = driver_results['points'].sum()
+        
+        total_wins = df_results[
+            (df_results['driverId'] == driver_id) & 
+            (df_results['position'] == '1')
+        ].shape[0]
+    else:
+        total_points = 0
+        total_wins = 0
+        driver_nationality = '-'
+    
 
-#Dados do piloto(Por enquanto só o total de pontos):
-col1, col2, col3, col4 = st.columns(4)
-with col1:
-    st.metric(label="Total de Pontos", value=f"{total_points:.0f}")
+#Mostra as informação do piloto:
+if category == "Piloto":
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.metric(label="Total de Pontos", value=f"{total_points:.0f}")
+    with col2:
+        st.metric(label="Vitórias", value=total_wins)
+    with col3:
+        st.metric(label="Nacionalidade", value=driver_nationality)
 
-#Gráfico de pontos por ano do piloto:
-if driver:
+#Gráfico de pontos por ano do piloto (Alterei para um gráfico de barras):
+if category == "Piloto" and driver:
     driver_points_by_year = (
-        #Filtro para pegar apenas o ano em que o piloto correu:
         df_results[df_results['driverId'] == driver_id]
-        #Coleta o ano da corrida:
         .merge(df_races[['raceId', 'year']], on='raceId')
-        #Agrupa os dados por ano e soma os pontos das corridas:
         .groupby('year')['points']
         .sum()
         .reset_index()
     )
 
-    #Criação do gráfico usando Plotly Express:
-    fig = px.line(
+    fig = px.bar(
         driver_points_by_year,
         x='year',
         y='points',
         title=f'Pontos por ano - {driver}',
-        markers=True
     )
-    #Atualização do gráfico:
     fig.update_layout(
         xaxis_title="Ano",
         yaxis_title="Pontos",
         showlegend=False
     )
     
-    #Exibição do gráfico usando Streamlit:
     st.plotly_chart(fig, use_container_width=True)
+
+#Observações: Ainda falta mostrar mais gráficos baseados nos dados dos pilotos e criar a parte das equipes.
